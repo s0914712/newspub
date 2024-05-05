41637693d92ffe8b758d685e49340f95a12a6183
s0914712 <143980460+s0914712@users.noreply.github.com>
2024-05-05 14:01:31 +0800

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -6,8 +6,6 @@ from selenium import webdriver
 headers = {
     	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
 	}
-    r = re
-
 def news_crawler():
 quests.get('https://www.cna.com.tw/search/hysearchws.aspx?q=海軍', headers=headers)
     base_url='https://www.cna.com.tw'
