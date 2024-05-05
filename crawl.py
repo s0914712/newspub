5eb2532b4401acb04993eab26637cdb74fc99440
s0914712 <143980460+s0914712@users.noreply.github.com>
2024-05-05 13:40:29 +0800

    Add files via upload

---
+++ crawl.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+headers = {
+    	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+	}
+    r = re
+
+def news_crawler():
+quests.get('https://www.cna.com.tw/search/hysearchws.aspx?q=海軍', headers=headers)
+    base_url='https://www.cna.com.tw'
+    soup = BeautifulSoup(r.text, 'html.parser')
+    anime_items = soup.select('li')
+    wrap = soup.find("img").get("alt")
+    #print(wrap)
+    all_title = []
+    for i in anime_items:
+      try:
+          all_title.append(i.find("img").get("alt"))
+          news_list_url = base_url + i.find("a").get("href")
+          all_title.append("\n")
+          all_title.append(news_list_url)
+          all_title.append("\n")
+      except:
+        continue
+    msg = ""
+    msg = "".join(map(str,all_title[69:96]))
+    return msg
+  
