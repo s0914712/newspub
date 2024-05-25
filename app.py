4329d81523df72942ba73e038a32901008f0866f
s0914712 <143980460+s0914712@users.noreply.github.com>
2024-05-25 14:57:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def handle_message(event):
                              
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=msg)
+            TextSendMessage(text=msg+event.reply_token)
         )
         
 
