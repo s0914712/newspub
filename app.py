fb7ac19de6ec05ce29344003786d94cfe262f051
s0914712 <143980460+s0914712@users.noreply.github.com>
2024-05-26 15:38:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -96,120 +96,120 @@ def handle_message(event):
          TextSendMessage(uid+"  "+new_uid) 
             )
     else:
-    line_bot_api.push_message(event.reply_token, FlexSendMessage(
-    alt_text='hello',
-    contents={ 
-      "type": "bubble",
-      "hero": {
-        "type": "image",
-        "url": "https://ocacnews.net/articleImages/202212/M2_0CE27D18-7BDD-5AEE-565B-CA45D4B96A79.png",
-        "size": "full",
-        "aspectRatio": "20:13",
-        "aspectMode": "cover",
-        "action": {
-          "type": "uri",
-          "uri": "https://line.me/"
-        }
-      },
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "動態查詢機器人",
-            "weight": "bold",
-            "size": "xl"
+        line_bot_api.push_message(event.reply_token, FlexSendMessage(
+        alt_text='hello',
+        contents={ 
+          "type": "bubble",
+          "hero": {
+            "type": "image",
+            "url": "https://ocacnews.net/articleImages/202212/M2_0CE27D18-7BDD-5AEE-565B-CA45D4B96A79.png",
+            "size": "full",
+            "aspectRatio": "20:13",
+            "aspectMode": "cover",
+            "action": {
+              "type": "uri",
+              "uri": "https://line.me/"
+            }
           },
-          {
+          "body": {
             "type": "box",
             "layout": "vertical",
-            "margin": "lg",
-            "spacing": "sm",
             "contents": [
               {
-                "type": "box",
-                "layout": "vertical",
-                "spacing": "sm",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "功能",
-                    "color": "#aaaaaa",
-                    "size": "sm",
-                    "flex": 1
-                  },
-                  {
-                    "type": "text",
-                    "text": "查動態、查新聞、掛號",
-                    "wrap": true,
-                    "color": "#666666",
-                    "size": "sm",
-                    "flex": 5
-                  }
-                ]
+                "type": "text",
+                "text": "動態查詢機器人",
+                "weight": "bold",
+                "size": "xl"
               },
               {
                 "type": "box",
                 "layout": "vertical",
+                "margin": "lg",
                 "spacing": "sm",
                 "contents": [
                   {
-                    "type": "text",
-                    "text": "使用方法",
-                    "color": "#aaaaaa",
-                    "size": "sm",
-                    "align": "start"
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "功能",
+                        "color": "#aaaaaa",
+                        "size": "sm",
+                        "flex": 1
+                      },
+                      {
+                        "type": "text",
+                        "text": "查動態、查新聞、掛號",
+                        "wrap": true,
+                        "color": "#666666",
+                        "size": "sm",
+                        "flex": 5
+                      }
+                    ]
                   },
                   {
-                    "type": "text",
-                    "text": "輸入：查詢   or 輸入：掛號+軍線",
-                    "wrap": true,
-                    "color": "#666666",
-                    "size": "sm",
-                    "flex": 5,
-                    "margin": "none",
-                    "action": {
-                      "type": "message",
-                      "label": "action",
-                      "text": "範例：查詢"
-                    }
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "使用方法",
+                        "color": "#aaaaaa",
+                        "size": "sm",
+                        "align": "start"
+                      },
+                      {
+                        "type": "text",
+                        "text": "輸入：查詢   or 輸入：掛號+軍線",
+                        "wrap": true,
+                        "color": "#666666",
+                        "size": "sm",
+                        "flex": 5,
+                        "margin": "none",
+                        "action": {
+                          "type": "message",
+                          "label": "action",
+                          "text": "範例：查詢"
+                        }
+                      }
+                    ]
                   }
                 ]
               }
             ]
-          }
-        ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "vertical",
-    "spacing": "sm",
-    "contents": [
-      {
-        "type": "button",
-        "style": "link",
-        "height": "sm",
-        "action": {
-          "type": "uri",
-          "label": "CALL",
-          "uri": "https://line.me/"
-        }
       },
-      {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [],
-        "margin": "sm"
+      "footer": {
+       "type": "box",
+       "layout": "vertical",
+       "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "style": "link",
+            "height": "sm",
+            "action": {
+              "type": "uri",
+              "label": "CALL",
+              "uri": "https://line.me/"
+            }
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [],
+            "margin": "sm"
+          }
+        ],
+        "flex": 0
       }
-    ],
-    "flex": 0
-  }
-}
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='收到')
-        )
+    }
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='收到')
+            )
         
 
 if __name__ == "__main__":
