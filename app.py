fcfd04eef1cd8147f40e3df07cf1967733588bf8
s0914712 <143980460+s0914712@users.noreply.github.com>
2024-05-26 09:37:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def handle_message(event):
          cursor.execute("SELECT * FROM userdata;")#選擇資料表userdata
          rows = cursor.fetchall() #讀出所有資料
          for row in rows:   #將讀到的資料全部print出來
-             text=str(row[0])+str(row[1])+str(row[2])+"\n"
+             text+=str(row[0])+str(row[1])+str(row[2])+"\n"
          line_bot_api.reply_message(
          event.reply_token,
          TextSendMessage(text) 
