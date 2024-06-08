1dc6d95af91f044bb0ab6754866ee9d87bdd584a
s0914712 <143980460+s0914712@users.noreply.github.com>
2024-06-08 10:42:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from chatgpt import *
 chatgpt = ChatGPT()
 app = Flask(__name__)
-
 Channel_Access_Token = '+rq5EEHCHR5pK6abD/3VuJZ8Q0iZxlb55AN6TzcBO6OC0f9buhiwdicHohpqPpnO8oHa0g/VHUl0AOz8q+yxkBoDmKSyuHZyQpUTQO8i93fI45O5CUdTnwiReYDSTKX+hUWM7Ye5uM0v4Zl61xz85gdB04t89/1O/w1cDnyilFU='
 line_bot_api    = LineBotApi(Channel_Access_Token)
 
