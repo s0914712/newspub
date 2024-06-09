8019e23c3ece516b81e3c5097003252d8b544864
s0914712 <143980460+s0914712@users.noreply.github.com>
2024-06-09 23:57:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -204,8 +204,8 @@ def handle_message(event):
             "flex": 0
           }
         }))
-        uid=":沒空"
-        new_uid=":有空"
+        uid="沒空"
+        new_uid="有空"
         if "更改狀態：參座有空" in msg:
             cursor = conn.cursor()
             cursor.execute(f"UPDATE userdata SET userid = '{new_uid}' WHERE userid = '{uid};")
