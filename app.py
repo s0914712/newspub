96cf9150b34b53c8347ff8954377ab2f125e6674
s0914712 <143980460+s0914712@users.noreply.github.com>
2024-06-09 23:53:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -206,10 +206,10 @@ def handle_message(event):
         }))
         uid=":沒空"
         new_uid=":有空"
-        if "沒空" in msg:
+        if "更改狀態：參座有空" in msg:
             cursor = conn.cursor()
             cursor.execute(f"UPDATE userdata SET userid = '{new_uid}' WHERE userid = '{uid};")
-        if "有空" in msg:
+        if "更改狀態：參座沒空" in msg:
             cursor = conn.cursor()
             cursor.execute(f"UPDATE userdata SET userid = '{uid}' WHERE userid = '{new_uid};")
         conn.commit()
