a076c513cb10edbfeefad4c0f09f655eca9cc51f
s0914712 <143980460+s0914712@users.noreply.github.com>
2024-06-09 21:45:12 +0800

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -118,7 +118,7 @@ class GPT_Cal:
         location = gcal_list[2] or 'TBC'
         desc = gcal_list[3] or 'TBC'
         gcal_url: str = create_gcal_url(title, date, location, desc)
-        return gcal_list[2] 
+        return gcal_url 
 
     def add_msg(self, text):
         self.text=text
