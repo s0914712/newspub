955ee508aac6652337b0d6cdc685a26ae7b1d663
s0914712 <143980460+s0914712@users.noreply.github.com>
2024-06-09 21:19:14 +0800

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -88,19 +88,19 @@ class ChatGPT:
         self.prompt.add_msg(text)
 class GPT_Cal:
     def __init__(self):
-        self.prompt = Prompt()
-        self.model = os.getenv("OPENAI_MODEL", default = "gpt-4")
+        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
         self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
         self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 500))
+        self.text="2014年三月五日在台北美麗華餐廳與同事聚餐"
             #Source 你會幫我把內容都轉換為 google calendar 的邀請網址。
             #Message 我會給你任何格式的訊息，需要整理裡面的內容並對應上google calendar 的渲染方式，中文字需要編碼，
             #Channel 如果開頭有"國會組報告"，那"預"字後的文字會有時間，"時"字後面會有內容，例如：國會組報告，國防部陸常次預5/9日(週四)，1400時於辦公室主持無人機預算說明。這代表"5/9(週四)，1400時"時間是20240509T1400，於辦公室代表地點在"立委辦公室"，後面的主持無人機預算說明代表內容為"陸常次主持預算說明會"將內容整理成標題、時間、地點、描述。範例: ['無人機預算說明', '202400509T140000/202400509T2330000', '委員辦公室', '具體描述']，並且要能整理出對應標題、行事曆時間、地點，其餘內容整理完後放在描述裡面，現在是 2024年，如果有兩筆資料中間會以分號；區隔，如果沒有分號(；)請依照語意分成另外一筆。
             #Receiver 連結google行事曆表單需要點選的民眾。
             #Effect 最後透過陣列回傳。
     def get_response(self):
-        text=self.prompt.generate_prompt()
+        text=self.text
         response = client.chat.completions.create(
-            model="gpt-4",
+            model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": f"""
             Source 你會幫我把內容都轉換為  google calendar 的邀請網址。
             Message 我會給你任何格式的訊息，需要整理裡面的內容並對應上google calendar 的渲染方式，中文字需要編碼，
@@ -112,24 +112,17 @@ class GPT_Cal:
         first_choice = response.choices[0]
         processed_text: str = response.choices[0].message.content
         gcal_list: list = ast.literal_eval(processed_text)
-        for i in range(2):
+        num_sentences = len(gcal_list)
+        for i in range(num_sentences):
             title = gcal_list[i][0] or 'TBC'
             date = gcal_list[i][1] or 'TBC'
             location = gcal_list[i][2] or 'TBC'
             desc = gcal_list[i][3] or 'TBC'
             gcal_url: str = create_gcal_url(title, date, location, desc)
         return gcal_url
-        line_bot_api.reply_message(
-        event.reply_token,
-        [
-            TextSendMessage(text="點選以下網址前，先確認時間地點："),
-            TextSendMessage(
-                text=f'標題: {title}\n時間: {date}\n地點: {location}\n描述: {desc}'),
-            response
-        ]
-    )
+
     def add_msg(self, text):
-        self.prompt.add_msg(text)
+        self.text=text
 
 # 從 Choice 物件中取得回應的內容
 
