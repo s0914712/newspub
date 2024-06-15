4b240eb1b97f6f0e11ad768aec82044479f51227
s0914712 <143980460+s0914712@users.noreply.github.com>
2024-06-15 16:58:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def handle_message(event):
   "type": "bubble",
   "hero": {
     "type": "image",
-    "url": "https://developers-resource.landpress.line.me/fx/img/01_1_cafe.png",
+    "url": ".\figgure.png",
     "size": "full",
     "aspectRatio": "20:13",
     "aspectMode": "cover",
